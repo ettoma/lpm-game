778e64523bf697293adc66daec095d9e79c0f5db
EttoreToma <ettore.toma@gmail.com>
2023-04-15 16:51:15 +0200

    Merge

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,13 @@ import level1 from "./scenes/1"
 import menu from "./scenes/menu"
 import Game from "./engine/engine"
 import { loader } from "./resources"
+import characterSelection from "./scenes/character_selection"
 
 
 
 Game.addScene("level1", level1)
 Game.addScene("menu", menu)
+Game.addScene("character_selection", characterSelection)
 
 
 Game.start(loader)
